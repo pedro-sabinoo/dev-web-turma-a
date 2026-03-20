2d96df74d99b3a51a55f876375705b4a5006bfd2
Pedro Henrique de Mello Sabino <pedro.sabino@dinfor.uniceub>
2026-03-19 21:17:05 -0300

    Atividade VUE CLI

---
+++ Pedro Sabino/Aulas/vue_cli/src/components/HomeComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <footer id="footer">
+            <p>Bem vindo: {{ nome }} Olá mundo do <b>VUE com CLI</b></p>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default{
+        name : "HomeComponent",
+        data() {
+            return {
+                nome : "P.diddy",
+            };
+        },
+    };
+</script>
